fc6b3bd09e0f3fb03b52c65854256ec45df410b4
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-07 06:48:29 -0500

    Add scale function to get bar height out of max height

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ fetch('https://raw.githubusercontent.com/MooseCowBear/frontend-mentor-expenses-c
   .catch(error => displayError(error)); //function for loading an error message in case it doesn't read
 
 const days = {sun: 0, mon: 1, tue: 2, wed:3, thu: 4, fri: 5, sat: 6};
+const maxHeight = 5.3;
+
 function displayData(data) {
     const today = new Date().getDay(); //returns 0 - 6, with 0 being sun - graph starts week on monday
     for (let i = 0; i < data.length; i++) {
         const day = data[i].day;
         const amount = data[i].amount; 
 
+
         //get the right bar
         const bar = document.getElementById(day);
-        bar.style.height = `${amount/9}em`; 
+
+        //need max value in the data 
+        const max = Math.max(...data.map(elem => elem.amount), 0);
+
+        const barHeight = scale(amount, max, maxHeight);
+
+        bar.style.height = `${barHeight}em`; 
 
         if (days[day] === today) {
             bar.classList.add("today");
@@ -21,6 +30,10 @@ function displayData(data) {
         //and the right amount div
         const amountDisplay = document.getElementById(`${day}-spent`);
         amountDisplay.innerText = "$" + amount; 
+    }
+
+    function scale(value, maxAmount, maxHeight) { 
+        return (value  * maxHeight) / maxAmount; 
     }
 }
 
@@ -43,7 +56,6 @@ bars.forEach(elem => elem.addEventListener('mouseleave', event => {
 }));
 
 //focusing with tabbing
-
 bars.forEach(elem => elem.addEventListener('focus', event => {
     const amountSpentDiv = event.target.previousElementSibling; 
     amountSpentDiv.classList.add("focused");
